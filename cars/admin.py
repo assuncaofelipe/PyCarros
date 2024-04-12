86cf762dc2031a1b47ff3d05bf310ea996b8bfea
Felipe Vasconcelos Assunção <felipe.assuncao@eldorado.org.br>
2024-04-12 09:51:49 -0400

    feat: configurando cars no django admin

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from cars.models import Car, BrandsCar
 
-# Register your models here.
+class BrandsCarAdmin(admin.ModelAdmin):
+    # verificar se existe uma maneira de pegar todos os campos por meio de parametro
+    list_display = ('name',)
+    search_fields = ('name',)
+
+class CarAdmin(admin.ModelAdmin):
+    # verificar se existe uma maneira de pegar todos os campos por meio de parametro
+    list_display = ('modelCar', 'brand', 'factory_year', 'modelCar_year', 'valueCar')
+    search_fields = ('modelCar', 'brand')
+
+admin.site.register(BrandsCar, BrandsCarAdmin)
+admin.site.register(Car, CarAdmin)
