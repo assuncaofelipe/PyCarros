d50eca6d9c3c8f2f92b238d348fba7b066db4863
Felipe Vasconcelos Assunção <felipe.assuncao@eldorado.org.br>
2024-04-18 11:05:40 -0400

    feat: criando templates e aplicando filtros de buscas

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 
+from cars.views import cars_view, new_car_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cars/',cars_view, name='cars_list' ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from cars.models import Car
+from django.db.models import Q
 
-# Create your views here.
+def cars_view(request):
+    cars = Car.objects.all().order_by('modelCar')
+    search = request.GET.get('search')
+    
+    if search:
+        cars = cars.filter(
+            Q(modelCar__icontains=search) | Q(brand__name__icontains=search)
+        )
+
+    return render( 
+        request,
+        'cars.html',
+        {'cars': cars}
+    )
