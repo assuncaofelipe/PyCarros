9d90600e18f9900877c14f7d6766e3a6394d7818
Felipe Vasconcelos Assunção <felipe.assuncao@eldorado.org.br>
2024-05-03 14:52:05 -0400

    refact: change simple funcitons to Class Based View

--- cars/views.py
+++ cars/views.py
@@ -2,33 +2,40 @@ from django.shortcuts import redirect, render
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.db.models import Q
+from django.views import View
 
-def cars_view(request):
-    cars = Car.objects.all().order_by('modelCar')
-    search = request.GET.get('search')
-    
-    if search:
-        cars = cars.filter(
-            Q(modelCar__icontains=search) | Q(brand__name__icontains=search)
+class CarsView(View):
+    def get(self, request):
+        cars = Car.objects.all().order_by('modelCar')
+        search = request.GET.get('search')
+        
+        if search:
+            cars = cars.filter(
+                Q(modelCar__icontains=search) | Q(brand__name__icontains=search)
+            )
+
+        return render( 
+            request,
+            'cars.html',
+            {'cars': cars}
         )
 
-    return render( 
-        request,
-        'cars.html',
-        {'cars': cars}
-    )
-
-def new_car_view(request):
-    if request.method.upper() == 'POST':
-        new_car_form = CarModelForm(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-    else:
+class NewCarView(View):
+    def get(self, request):
         new_car_form = CarModelForm()
-    return render(
+        return render(
             request, 
             'new_car.html', 
             { 'new_car_form': new_car_form }
         )
     
+    def post(self, request):
+        new_car_form = CarModelForm(request.POST, request.FILES)
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('cars_list')
+        return render(
+            request, 
+            'new_car.html', 
+            { 'new_car_form': new_car_form }
+        )
